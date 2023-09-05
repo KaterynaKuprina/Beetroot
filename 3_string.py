58f0d7ea4a333b820fd35494552a204cddf1b65c
KaterynaKuprina <kateryna.morhun88@gmail.com>
2023-09-05 23:21:03 +0300

    HW string

---
+++ 3_string.py
@@ -0,0 +1,25 @@
+# task 1
+name = "Kateryna"
+day = "Monday"
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+
+print("Good day" + " " + name + "! " + day + " is a perfect day to learn some python.")
+
+
+# task 2
+first_name = "Kateryna "
+second_name = "Morhun "
+greeting = first_name + second_name
+print("Hello, " + greeting)
+
+# task 3
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(abs(b - a))
+print(a // b)
