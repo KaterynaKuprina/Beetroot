40920cbf253109d51e2304ad1e64f13305530d9a
KaterynaKuprina <kateryna.morhun88@gmail.com>
2023-09-26 19:04:59 +0300

    homework 9

---
+++ HW_9/task_1/part_2.py
@@ -0,0 +1,5 @@
+from part_1 import *
+
+sum_numbers(4, 7)
+mult_numbers(3, 4) # просто эксперимент c недоступной функцией
+
---
+++ HW_9/task_2/module_1.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.path)
+sys.path.append('C:/Users/Катерина/Desktop/Beetroot/HW_9/task_1')
+
+from part_1 import sum_numbers as summ
+
+summ(22, 14)
+
